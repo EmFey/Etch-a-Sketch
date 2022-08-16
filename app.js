567d5cd43948bc320e04a973a22f1f01dc2e7df6
EmFey <manufety15@gmail.com>
2022-08-16 19:49:48 +0200

    Modifying the js to work with flex

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const container = document.querySelector(".container")
 
-function createRows(rows, cols) {
+for (let i = 1; i < 257; i++) {
+    const div = document.createElement('div');
+    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";  
+    container.appendChild(div);
+}
+
+/*function createRows(rows, cols) {
     container.style.setProperty('.row', rows);
     container.style.setProperty('.cell', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    for (i = 0; i < (rows * cols); i++) {
       let cell = document.createElement("div");
-      cell.textContent = (c + 1);
-      container.appendChild(cell).className = "grid-item";
+      cell.textContent = (i + 1);
+      container.appendChild(cell).className = "row";
     };
 };
   
-createRows(16, 16);
+createRows(16, 16);*/
