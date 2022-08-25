a29883f51f5888707002021cae136a3cdf27f93d
EmFey <manufety15@gmail.com>
2022-08-25 17:51:19 +0200

    Changed the divs to display as grid instead of flex

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ btnAdd.addEventListener("click", expandGrid);
 btnReset.addEventListener("click", resetGrid);
 
 function createCanvas(rowVcol) {
+    let squares = container.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${rowVcol} , 1fr)`;
+    container.style.gridTemplateRows = `repeat(${rowVcol} , 1fr)`;
+
     for (let i = 0; i < (rowVcol * rowVcol); i++) {
         const div = document.createElement('div');
         
@@ -24,6 +29,9 @@ function createCanvas(rowVcol) {
 function expandGrid() {
     container.innerText = "";
     let increase = Number(prompt("Enter the size of your grid (< 100):"));
+    div.style.width = "100%";
+    let height = 400 / parseInt(increse);  
+    div.style.height = `${height}px`; 
     createCanvas(increase);
 }
 
